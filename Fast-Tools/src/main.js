4c62a7e213947f36667fe7e020697a295f9827fe
sebi-vscode2 <stoklosasebastian114@gmail.com>
2022-09-16 21:20:43 +0000

    Remove the folder Fast-Tools

--- Fast-Tools/src/main.js
+++
@@ -1,5 +0,0 @@
-const { invoke } = window.__TAURI__.tauri;
-import { Command } from '@tauri-apps/api/shell'
-const install = new Command('Installing-FastMc')
-
-command.spawn()
---
+++ src/main.js
@@ -0,0 +1,5 @@
+const { invoke } = window.__TAURI__.tauri;
+import { Command } from '@tauri-apps/api/shell'
+const install = new Command('Installing-FastMc')
+
+command.spawn()
